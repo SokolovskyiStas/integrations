493d114ac637654d2d313b91a5c5817191c71c04
Stanislav Sokolovskyi <stanislav.sokolovskyi@getbase.com>
2018-12-24 09:17:52 +0100

    wip

--- app/src/main/java/com/ssokolovskyi/integrations/MainActivity.kt
+++ app/src/main/java/com/ssokolovskyi/integrations/MainActivity.kt
@@ -2,11 +2,46 @@ package com.ssokolovskyi.integrations
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.EditText
+import android.Manifest
+
+/*
+* Issues:
+* - Formatting
+* - Unused imports
+* - Deprecated method calls
+* - Unused resources
+* - Hardcoded string
+* - Duplicated code
+* */
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        titleTextView = findViewById(R.id.title)
+        deprecatedMethod() //deprecated method
+                invalidateOptionsMenu() //formatting
+        titleTextView.text = "Hardcoded string"
+    }
+
+    @Deprecated(message = "Deprecated")
+    private fun deprecatedMethod() {
+    }
+
+    private fun updateUI() {
+        titleTextView.invalidate()
+        invalidateOptionsMenu()
+        return Unit
+    }
+
+    private fun refreshUI() {
+        titleTextView.invalidate()
+        invalidateOptionsMenu()
+        return Unit
     }
 }

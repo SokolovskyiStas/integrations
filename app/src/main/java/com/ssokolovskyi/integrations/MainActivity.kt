9eb1e13cc4e163c0eae32c0887d9fb79f3881b98
Stanislav Sokolovskyi <stanislav.sokolovskyi@getbase.com>
2018-12-24 09:17:52 +0100

    wip

--- app/src/main/java/com/ssokolovskyi/integrations/MainActivity.kt
+++ app/src/main/java/com/ssokolovskyi/integrations/MainActivity.kt
@@ -2,11 +2,57 @@ package com.ssokolovskyi.integrations
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.EditText
+import android.Manifest
+
+/*
+* Issues:
+* - Formatting
+* - Unused imports
+* - Deprecated method calls
+* - Unused resources
+* - Hardcoded string
+* - Duplicated code
+* */
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+private lateinit var titleTextView: TextView
+
+init {
+
+}
+
+override fun onCreate(savedInstanceState: Bundle?) {
+super.onCreate(savedInstanceState)
+setContentView(R.layout.activity_main)
+titleTextView = findViewById(R.id.title)
+deprecatedMethod() //deprecated method
+invalidateOptionsMenu() //formatting
+titleTextView.text = "Hardcoded string"
+when {
+
+}
+}
+
+@Deprecated(message = "Deprecated")
+private fun deprecatedMethod() {
+}
+
+final fun emptyFunctionBlog() {
+
+}
+
+private fun updateUI() {
+titleTextView.invalidate()
+invalidateOptionsMenu()
+return Unit
+}
+
+private fun refreshUI() {
+titleTextView.invalidate()
+invalidateOptionsMenu()
+return Unit
+}
 }
